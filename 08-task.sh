4b9a29c0aaabf2c3bd5604f9c6eedaa1c232b62f
Chandra <diponugotichandu86@gmail.com>
2024-03-19 18:25:19 +0530

    task3

--- 08-task.sh
+++ 08-task.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Check whether root user or not
-USER=$(id -u)
-if [ $USER -ne 0 ]
-then 
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then
     echo "execute command as root user"
 else
     sh sudo yum install mysql -y
