87ec70dc9a5b76de4a2e6f99384db333acfcb616
Chandra <diponugotichandu86@gmail.com>
2024-03-19 17:38:02 +0530

    var4

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 persion1=Ramesh
 persion2=Suresh
-DATE = $(date)
+DATE=$(date)
 
 echo "$person1 Hello, $persion2 How are you"
 echo "Hi $persion1, Im good Thank you"
-echo "Execution time is $DATE"
+echo "Execution time is: $DATE"
