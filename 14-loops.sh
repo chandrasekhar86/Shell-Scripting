3d3b4b92265d5d94f5f30d330f70ddbe30c9155e
Chandra <diponugotichandu86@gmail.com>
2024-03-23 14:04:15 +0530

    vars

--- 14-loops.sh
+++ 14-loops.sh
@@ -18,13 +18,13 @@ then
 fi
 
 VALIDATE (){
-if [ $1 -ne 0 ];
-then
-    echo -e "Installing $2 ... $R FAILURE $N"
-    exit 1
-else
-    echo -e "Installing $2 ... $G SUCCESS $N"
-fi
+    if [ $1 -ne 0 ];
+    then
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
 }
 # all args are in $@
 for i in $@
